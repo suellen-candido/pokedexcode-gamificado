46916417b6e3f530a870753e405d5a6e4c124c83
PabloCaioSantos <pablo@email.com>
2026-01-18 19:35:58 -0300

    Suellen - E claro que eu não poderia me esquecer de colocar o nome do GOAT, Anderson Uchôa!!!

--- app/api/classes/me/route.ts
+++ app/api/classes/me/route.ts
@@ -39,7 +39,7 @@ export async function GET(request: NextRequest) {
     const enrichedClassData = {
       ...classInfo,
       professor: {
-        name: professorProfile?.profile.name || 'Desconhecido',
+        name: professorProfile?.profile.name || 'Anderson Uchôa',
         avatar: professorProfile?.profile.avatar || '',
       }
     };
